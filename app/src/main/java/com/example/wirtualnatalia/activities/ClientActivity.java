cd034f2df80f5ad9b8304af71feb4d629d0d5630
Dawid Jakobczak <dawidjakobczakbn@gmail.com>
2020-11-05 23:46:44 +0100

    basic http client-server
    This commit adds simple implementation of http server and client.
    Additionally the simplest form of network service discovery is
    added.

---
+++ app/src/main/java/com/example/wirtualnatalia/activities/ClientActivity.java
@@ -0,0 +1,62 @@
+package com.example.wirtualnatalia.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.wirtualnatalia.R;
+import com.example.wirtualnatalia.network.server.HTTPClient;
+
+
+public class ClientActivity extends Activity {
+    private Button sndGetBtn;
+    private Button sndPostBtn;
+    private TextView responseView;
+    private TextView responseTxt;
+
+    private String STATUS_URL = "http://192.168.0.24:5050/status";
+
+    private HTTPClient client;
+
+    private static final String TAG = "ClientActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+
+        sndGetBtn = (Button) findViewById(R.id.sndGetBtn);
+        sndGetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleGetStatus();
+            }
+        });
+
+        sndPostBtn = (Button) findViewById(R.id.sndPostBtn);
+        sndPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handlePostStatus();
+            }
+        });
+
+        responseView = (TextView) findViewById(R.id.responeView);
+
+        client = new HTTPClient();
+    }
+
+    public void handleGetStatus() {
+        Log.d(TAG, "Send GET request to the server...");
+        client.sendStatusGET(this, responseView);
+    }
+
+    public void handlePostStatus() {
+        Log.d(TAG, "Send POST request to the server...");
+        client.sendStatusPOST(this, responseView);
+    }
+
+}
